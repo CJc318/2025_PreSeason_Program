78be174eeb8145c8699ff42daa8ac4a42d6299c6
Apple Pi <applePiRobot@gmail.com>
2024-11-13 07:42:28 -0500

    added intake and shooter subsytem as well as reformatted controls

--- src/main/java/frc/robot/commands/FalconRun.java
+++ src/main/java/frc/robot/commands/FalconRun.java
@@ -1,16 +1,17 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.subsystems.Falcon;
+import frc.robot.subsystems.Shooter;
 
-public class FalconRun extends InstantCommand {
-    private final Falcon m_falcon;
+public class FalconRun extends Command {
+    private final Shooter m_falcon;
     private final double m_rpm;
 
     public FalconRun(double rpm) {
         m_rpm = rpm;
 
-        m_falcon = Falcon.getInstance();
+        m_falcon = Shooter.getInstance();
         addRequirements(m_falcon);
     }
 
@@ -18,4 +19,6 @@ public class FalconRun extends InstantCommand {
     public void initialize(){
         m_falcon.setTargetVelocityRPM(m_rpm);
     }
+
+
 }
